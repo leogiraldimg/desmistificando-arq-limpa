7698ffbded491f6d3c95ac71e332f96184ea14ba
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-08-22 08:03:50 -0300

    test: implementa testes unitários
    Este commit:
    
    - Implementa os testes unitários do controlador de tarefas para o spring
    - Sobrescreve os métodos equals e hashCode dos modelos utilizados nos testes

--- src/main/java/giraldi/dev/adapters/common/models/web/WebRequestModel.java
+++ src/main/java/giraldi/dev/adapters/common/models/web/WebRequestModel.java
@@ -1,9 +1,29 @@
 package giraldi.dev.adapters.common.models.web;
 
+import java.util.Objects;
+
 public class WebRequestModel<Params, Headers, Body, Query> {
 
     public Params params;
     public Headers headers;
     public Body body;
     public Query query;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        WebRequestModel<?, ?, ?, ?> that = (WebRequestModel<?, ?, ?, ?>) o;
+        return Objects.equals(params, that.params) &&
+                Objects.equals(headers, that.headers) &&
+                Objects.equals(body, that.body) &&
+                Objects.equals(query, that.query);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(params, headers, body, query);
+    }
 }
