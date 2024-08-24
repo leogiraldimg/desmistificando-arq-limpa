68622f896ffbedae9b98abec408754a0dcdce2cd
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-08-24 19:31:32 -0300

    test: implementa testes de integração

--- src/test/java/giraldi/dev/infra/web/spring/controllers/domain/task/TaskControllerTestsIT.java
+++ src/test/java/giraldi/dev/infra/web/spring/controllers/domain/task/TaskControllerTestsIT.java
@@ -1,7 +1,8 @@
 package giraldi.dev.infra.web.spring.controllers.domain.task;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 
@@ -17,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import giraldi.dev.adapters.common.models.web.WebRequestModel;
 import giraldi.dev.adapters.domain.task.create.models.web.CreateTaskWebBodyModel;
 import giraldi.dev.entities.domain.task.TaskStatus;
 
@@ -52,8 +52,6 @@ public class TaskControllerTestsIT {
 
     @Test
     public void insertShouldReturnCreatedTask() throws Exception {
-        WebRequestModel<Void, Void, CreateTaskWebBodyModel, Void> expected = new WebRequestModel<Void, Void, CreateTaskWebBodyModel, Void>();
-        expected.body = createTaskWebBodyModel;
         String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
 
         ResultActions result = mockMvc.perform(post("/tasks")
@@ -68,4 +66,110 @@ public class TaskControllerTestsIT {
         result.andExpect(jsonPath("$.content.dueDate").value(dueDate.toString()));
         result.andExpect(jsonPath("$.content.status").value(taskStatus.toString()));
     }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenTitleIsNotGiven() throws Exception {
+        createTaskWebBodyModel.title = null;
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message").value("Título de tarefa é obrigatório"));
+    }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenTitleIsLessThanThreeCharacters() throws Exception {
+        createTaskWebBodyModel.title = "a".repeat(2);
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(
+                jsonPath("$.message").value("Título de tarefa inválido. Título deve conter pelo menos 3 caracteres"));
+    }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenDescriptionIsNotGiven() throws Exception {
+        createTaskWebBodyModel.description = null;
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message").value("Descricão de tarefa é obrigatório"));
+    }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenDescriptionIsLessThanTenCharacters() throws Exception {
+        createTaskWebBodyModel.description = "a".repeat(9);
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(
+                jsonPath("$.message")
+                        .value("Descricão de tarefa inválida. Descricão deve conter pelo menos 10 caracteres"));
+    }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenDueDateIsNotGiven() throws Exception {
+        createTaskWebBodyModel.dueDate = null;
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message").value("Data de vencimento é obrigatório"));
+    }
+
+    // TODO: implementar teste de data no formato inválido
+
+    @Test
+    public void insertShouldReturnBadRequestWhenDueDateIsInThePast() throws Exception {
+        createTaskWebBodyModel.dueDate = LocalDate.now().minusDays(1);
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(
+                jsonPath("$.message").value("Data de vencimento inválida. Data deve ser posterior a data atual"));
+    }
+
+    @Test
+    public void insertShouldReturnBadRequestWhenStatusIsNotGiven() throws Exception {
+        createTaskWebBodyModel.status = null;
+        String jsonBody = objectMapper.writeValueAsString(createTaskWebBodyModel);
+
+        ResultActions result = mockMvc.perform(post("/tasks")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message").value("Status de tarefa é obrigatório"));
+    }
+
+    // TODO: implementar teste de valor inválido para status
 }
