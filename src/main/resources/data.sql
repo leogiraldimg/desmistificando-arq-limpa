9e291944838ebca04c9d1182f18aa76cacb860fc
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-08-24 19:29:31 -0300

    chore: configura seeding de tasks

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tasks (title, description, due_date, status, created_at, updated_at) VALUES
+('Task 1', 'Description 1', '2022-01-01', 'DONE', NOW(), NOW()),
+('Task 2', 'Description 2', '2022-01-02', 'DONE', NOW(), NOW()),
+('Task 3', 'Description 3', '2022-01-03', 'DONE', NOW(), NOW()),
+('Task 4', 'Description 4', '2022-01-04', 'DONE', NOW(), NOW()),
+('Task 5', 'Description 5', '2022-01-05', 'DONE', NOW(), NOW());
