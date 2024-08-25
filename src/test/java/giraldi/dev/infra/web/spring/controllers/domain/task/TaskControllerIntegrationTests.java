1edb24a94fcaa5de0e8e7842dd6794ba5334dc5b
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-08-25 09:54:00 -0300

    refactor: renomeia arquivo de testes

--- src/test/java/giraldi/dev/infra/web/spring/controllers/domain/task/TaskControllerIntegrationTests.java
+++ src/test/java/giraldi/dev/infra/web/spring/controllers/domain/task/TaskControllerIntegrationTests.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class TaskControllerTestsIT {
+public class TaskControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
